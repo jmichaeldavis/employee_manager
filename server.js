c9e9fc367cacd9dccb60355e961812dab221b21e
jmichaeldavis <jmichaeldavis17@gmail.com>
2023-06-19 19:36:17 -0600

    adding the js functionality

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ const db = mysql.createConnection(
   console.log('Connected to Database.')
 );
 
+function latestAllDepartmentsArray() {
+  db.query("SELECT * FROM department", function (err, results){
+    if (err) {
+      console.log(err)
+      return []
+    }
+    return results;
+  })
+}
+
+let allDepartments = latestAllDepartmentsArray()
 
 function displayMenu() {
   //Presents the options through inquirer
@@ -27,9 +38,26 @@ function displayMenu() {
     if(data.menuChoice === "view all departments"){
       viewAllDepartments()
     }
-    console.log(data.menuChoice);
+    if(data.menuChoice === "view all roles"){
+      viewAllRoles()
+    }
+    if(data.menuChoice === "view all employees"){
+      viewAllEmployees()
+    }
+    if(data.menuChoice === "add a department"){
+      addDepartment()
+    }
+    if(data.menuChoice === "add a role"){
+      console.log("not ready")
+    }
+    if(data.menuChoice === "add an employee"){
+      console.log("not ready")
+    }
+    if(data.menuChoice === "update an employee role"){
+      console.log("not ready")
+    }
   })
-}
+};
 function viewAllDepartments() {
   db.query("SELECT * FROM department", function (err, results){
     if (err) {
@@ -39,8 +67,77 @@ function viewAllDepartments() {
     console.table(results)
   })
 }
+function viewAllRoles() {
+  db.query("SELECT * FROM role", function (err, results){
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.table(results)
+  })
+}
+function viewAllEmployees() {
+  db.query("SELECT * FROM employee", function (err, results){
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.table(results)
+  })
+}
+function addDepartment() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What department do you want to add?',
+      name: 'newDepartment',
+    }
+  ])
+  .then((data) => {
+    db.query(`INSERT INTO department (name) VALUES ("${data.newDepartment}")`, function (err, results){
+      if (err) {
+       console.log(err)
+       return
+      }
+      viewAllDepartments()
+    })
+  })
+}
+function addRole() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What role do you want to add?',
+      name: 'newRole',
+    },
+    {
+      type: 'list',
+      message: 'What department is this for?',
+      name: 'departmentName',
+      choices: [allDepartments[i]]
+    }
 
-displayMenu();
+  ])
+  .then((data) => {
+    db.query(`INSERT INTO role (name) VALUES ("${data.newDepartment}")`, function (err, results){
+      if (err) {
+       console.log(err)
+       return
+      }
+      viewAllDepartments()
+    })
+  })
+}
+function getDepartmentOptions() {
+  let departmentOptions = []
+  for(i = 0; i < allDepartments.length; i++) {
+    departmentOptions.push(allDepartments[i].name)
+  }
+}
+console.log(allDepartments);
+//getDepartmentOptions()
+//displayMenu();
+//console.log(allDepartments);
 //init function calls selection function. Thats where the inquirer options begin. It will create the prompt lists. If prompt answer
 // === option run the db.query function. console.table shows the results in table format. would need to install with npm i
 // Need selection functions (display all, selection, etc)
